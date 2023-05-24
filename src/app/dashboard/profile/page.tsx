39f8c6ec463c822ef43131a637b632e4c55d8b71
OscarDRT <oscarnetworkingpro@gmail.com>
2023-05-23 23:42:56 -0500

    feat: remove semi

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Profile = () => {
   return (
     <section className="flex h-screen">
-      <h1>Esta es la página de perfil</h1>;
+      <h1>Esta es la página de perfil</h1>
     </section>
   )
 }
